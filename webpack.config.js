863438ee0319910e6390bb83782e75712e8451f8
calledT <huanggengtao@gmail.com>
2016-09-23 16:13:14 +0800

    Tweaks dev env

--- webpack.config.js
+++ webpack.config.js
@@ -56,12 +56,7 @@ module.exports = {
       },
     ]
   },
-  resolve: {
-    extensions: ['', '.js', '.scss'],
-    alias: {
-      'base64': path.resolve(__dirname, 'src/js/base64.js')
-    }
-  },
+  resolve: {extensions: ['', '.js', '.scss']},
   postcss: [autoprefixer(autoprefixerConfig)],
   devServer: {
     contentBase: path.resolve(__dirname, 'dist'),
