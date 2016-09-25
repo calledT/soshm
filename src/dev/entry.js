e151616e627e5e609483459b4a1edf4b92e3557e
calledT <huanggengtao@gmail.com>
2016-09-25 10:48:11 +0800

    remove master res

--- src/dev/entry.js
+++
@@ -1,7 +0,0 @@
-var Soshm = require('../js/index');
-
-var share1 = new Soshm('#share', {sites: ['weixin', 'weibo', 'yixin', 'qzone', 'tqq']});
-
-document.getElementById('btn1').addEventListener('click', function() {
-  share1.popIn({sites: ['weixin', 'weixintimeline', 'weibo', 'yixin', 'qzone', 'tqq', 'qq', 'renren', 'douban']});
-}, false);
